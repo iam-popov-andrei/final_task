9d0877be009c3b85d8d346fe9cc03f23389c5d50
iam-popov-andrei <280790pao@gmail.com>
2024-12-03 23:22:47 +0700

    final

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,4 @@ class ProductPageLocators():
     
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "input.btn")
